104a910ac83474e30b2ac0577c9ff3f4fdeb3963
SazanamiInko <s.suzuki1614@gmail.com>
2024-02-08 18:00:00 +0900

    if文

---
+++ src/pg0030_if/src/main.rs
@@ -0,0 +1,23 @@
+////////////////////////////////
+///
+///If
+/// 
+//////////////////////////////////
+
+//使用標準ライブラリ
+use std::env;
+
+// . メイン関数
+fn main() {
+
+    //入力
+    let mut c_count=0;
+    //とりあえずvectorは置いといていて
+    let args: Vec<String> = env::args().collect();
+    c_count=args.len()-1;
+
+    if(c_count==2)
+    {
+        println!("処理します")
+    }
+}
