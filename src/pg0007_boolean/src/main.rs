afdc68870107a26a522f95ce4fd2ac12aa234322
SazanamiInko <s.suzuki1614@gmail.com>
2024-02-08 16:11:17 +0900

    論理型

---
+++ src/pg0007_boolean/src/main.rs
@@ -0,0 +1,18 @@
+/////////////////////////////////
+///
+///論理型
+/// 
+//////////////////////////////////
+
+/// . メイン関数
+
+fn main() {
+    //型宣言
+    let val_false :bool=false;
+    let val_true :bool=true;
+ 
+    //値の表示
+    println!("val_false={}",val_false);
+    println!("val_true={}",val_true);
+ 
+ }
