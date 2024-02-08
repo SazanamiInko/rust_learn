12b5b15e215942f980596764d85d48cdf37c216d
SazanamiInko <s.suzuki1614@gmail.com>
2024-02-08 18:11:08 +0900

    分岐（他条件）

---
+++ src/pg00032_elseif/src/main.rs
@@ -0,0 +1,32 @@
+ ////////////////////////////////
+///
+///Ifelse
+/// 
+//////////////////////////////////
+
+//使用標準ライブラリ
+use std::env;
+
+// . メイン関数
+fn main() {
+
+    //入力
+    let mut c_count=0;
+    //とりあえずvectorは置いといていて
+    let args: Vec<String> = env::args().collect();
+    c_count=args.len()-1;
+
+    if(c_count==2)
+    {
+        println!("処理します")
+    }
+    else if(c_count==3)
+    {
+        println!("SPモードで処理します")
+
+    }
+    else
+    {
+        println!("引数が無効です")
+    }
+}
