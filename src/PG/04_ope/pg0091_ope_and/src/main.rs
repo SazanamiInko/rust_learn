717f7f74cc612a736b7aac3e52428e7ec5b27120
SazanamiInko <s.suzuki1614@gmail.com>
2024-03-24 19:37:07 +0900

    集合演算

---
+++ src/PG/04_ope/pg0091_ope_and/src/main.rs
@@ -0,0 +1,39 @@
+/////////////////////////////////
+///
+/// and演算
+/// 
+//////////////////////////////////
+
+//利用標準ライブラリ
+use std::io;
+
+//メイン関数
+fn main() {
+    let mut n=9;
+   
+    read_std_in(&mut n);
+
+    if n>0&&n<10
+    {       
+        println!("モード{}で運転します",n);
+        return;
+    }
+
+    println!("{}はデータが範囲外です",n);
+}
+
+// 標準入力を行います
+/// *message` - メッセージ
+/// *in_param` - パラメータ
+fn read_std_in(in_param:&mut i32)
+{
+    let mut buff:String=String::new();
+ 
+    println!("運転モードを入力してください");
+
+    io::stdin()
+        .read_line(&mut buff)
+        .expect("入力でエラーが発生しました");
+ 
+    *in_param= buff.trim().parse().expect("入力でエラーが発生しました");
+}
