707221ad1f1815358b244de55f86f4c8277de227
SazanamiInko <s.suzuki1614@gmail.com>
2024-06-08 17:03:25 +0900

    文字列のアペンド

---
+++ src/PG/15_String/pg0127_str_push/src/main.rs
@@ -0,0 +1,18 @@
+/////////////////////////////////
+/// 
+/// StringBuilder
+/// 
+/// //////////////////////////////
+ 
+///メイン関数
+fn main() {
+    
+    let mut uta=String::from("");
+    uta.push_str("あまつ風");
+    uta.push_str("雲のかよひ路");
+    uta.push_str("吹きとぢよ");
+    uta.push_str("をとめの姿");
+    uta.push_str("しばしとどめむ");
+
+    println!("{}",uta);
+}
