3f289068b3230c83a6c3a7546f6d909c0652a88e
SazanamiInko <s.suzuki1614@gmail.com>
2024-07-12 20:12:09 +0900

    マスターカードユーテリティ

---
+++ src/work/pg0137_db_insert/src/logics/common/master_card.rs
@@ -0,0 +1,36 @@
+////////////////////////////////////////////
+/// 
+///マスターカードユーテリティ
+///
+/// ////////////////////////////////////////
+use crate::logics::dao::master_card as dao_card;
+use mysql::*;
+
+///登録済みチェック
+pub fn has_master(m_id:&str,tran:&mut Transaction)->bool
+{
+    let card=dao_card::MasterCard::from(m_id,tran);
+
+    match card{
+        None=>{return false;}
+        Some(_)=>{return true;}
+    }
+}
+
+///承認者権有無
+pub fn is_auth(m_id:&str,tran:&mut Transaction)->bool
+{ 
+    let card=dao_card::MasterCard::from(m_id,tran);
+
+    match card{
+        None=>{return false;}
+        Some(card_data)=>
+        {
+            if !card_data.confirm_auth
+            {
+                return false;
+            }  
+        }
+    }
+    return true; 
+}
