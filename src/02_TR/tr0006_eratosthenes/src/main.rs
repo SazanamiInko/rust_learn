7edbc167e5e3a3099c6608d5f7bf7c7fd8e7f152
SazanamiInko <s.suzuki1614@gmail.com>
2025-03-24 14:56:49 +0900

    エラス地手ネスのふるいがソースがなかったので修正

--- src/02_TR/tr0006_eratosthenes/src/main.rs
+++ src/02_TR/tr0006_eratosthenes/src/main.rs
@@ -1,3 +1,40 @@
+use std::sync::TryLockResult;
+
+////////////////////////////////
+/// 
+/// エラトステネスのふるい
+/// 
+/// ////////////////////////////
+
+///主関数
 fn main() {
-    println!("Hello, world!");
+
+    let mut mark: [bool; 100]=[true;100];
+    let range=mark.len()/2;
+
+    for  n in 1..range
+    {
+        if n==1
+        {
+            continue;
+        }
+    
+        if mark[n]
+        {
+            let mut m=2*n;
+            while m<=mark.len()-1
+            {
+                mark[m]=false;
+                m=m+n;
+            }
+        }
+    }
+
+    for p in 1..mark.len()
+    {
+        if mark[p]
+        {
+            println!("{}",p);
+        }
+    }
 }
